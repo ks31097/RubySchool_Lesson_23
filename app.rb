8f2c96bb9ae2d07cc9fb260f7b36dc00aea62ec1
Kostya Sigarov <ksigaryov@gmail.com>
2022-04-17 13:13:23 +0300

    Add color picker

--- app.rb
+++ app.rb
@@ -15,16 +15,17 @@ get '/visit' do
 end
 
 post '/visit' do
-        @user_name = params[:user_name] # получить, то что отправил браузер
-        @user_phone = params[:user_phone] # получить, то что отправил браузер
-        @date_time = params[:date_time] # получить, то что отправил браузер
-        @master = params[:master] # получить, то что отправил браузер
+        @user_name = params[:user_name] # получить, то что отправил браузер со страницы visit.erb <input name="user_name" type="text" class="form-control" placeholder="Введите Ваше имя">
+        @user_phone = params[:user_phone] # получить, то что отправил браузер со страницы visit.erb <input name="user_phone" type="text" class="form-control" placeholder="Номер Вашего телефона">
+        @date_time = params[:date_time] # получить, то что отправил браузер со страницы visit.erb <input name="date_time" type="text" class="form-control" placeholder="Введите дату и время">
+        @master = params[:master] # получить, то что отправил браузер со страницы visit.erb <select name="master" class="form-select" aria-label="Default select example">
+        @color = params[:color] # получить, то что отправил браузер со страницы visit.erb <select id="color" name="color">
 
         @title = 'Спасибо!'
-        @message = "#{@user_name}, мы Вас ждем #{@date_time}. Ваш мастер #{@master}!"
+        @message = "#{@user_name}, мы Вас ждем #{@date_time}. Ваш мастер #{@master}, цвет окраски волос: #{@color}!"
 
         output = File.open('./public/visits.txt', 'a')
-        output.write "User: #{@user_name}, Phone: #{@user_phone}, Date and time: #{@date_time}, master: #{@master}\n"
+        output.write "User: #{@user_name}, Phone: #{@user_phone}, Date and time: #{@date_time}, master: #{@master}, color: #{@color}\n"
         output.close
 
         erb :message
